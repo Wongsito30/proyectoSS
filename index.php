50e383dd962c046c4f2c978a8f24aa026dbd55f3
Wongsito30 <sahirwong9803@gmail.com>
2024-02-12 00:09:32 -0700

    dios?

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Gestor de chequeos de enfermerias</title>
+</head>
+<body>
+    <img class="logo" src="img/logoservicio.png" alt="">
+    <img class="logo2" src="img/logouas.jpg" alt="">
+    <h1>Servicio social UAS</h1>
+    <form class="principal">
+        <label for="">Fecha de atencion: </label>
+        <input type="date">
+        <input type="text" placeholder="Nombre del paciente">
+        <input type="text" placeholder="Edad del paciente">
+        <input type="text" placeholder="Peso">
+        <input type="text" placeholder="Estatura">
+        <input type="text" placeholder="Presion arterial">
+        <input type="text" placeholder="SpO2">
+        <input type="text" placeholder="Temperatura">
+        <input type="text" placeholder="Glucosa">
+        <input type="text" placeholder="Frecuencia Respiratoria">
+        <input type="text" placeholder="Frecuencia Cardiaca">
+        <textarea name="sintomas" id="" cols="30" rows="10" placeholder="sintomas"></textarea>
+        <button>Enviar a pdf</button>
+    </form>
+</body>
+</html>
